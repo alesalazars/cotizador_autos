0b8367e585ac49df7e4c50eeacc86284bdabf328
Ale <ma.salazarsanchez@gmail.com>
2020-05-28 00:15:17 -0400

    Submit button calculates value

--- js/app.js
+++ js/app.js
@@ -5,6 +5,48 @@ function Seguro(marca, anio, tipo){
   this.tipo = tipo;
 }
 
+Seguro.prototype.cotizarSeguro = function(){
+  /* 
+    leer la marca:
+
+    1 = americano 1.15
+    2 = asiatico 1.05
+    3 = europeo 1.35
+  */
+  let cantidad;
+  const base = 2000;
+
+  switch(this.marca){
+    case '1':
+      cantidad = base * 1.15;
+      break;
+    case '2':
+      cantidad = base * 1.05;
+      break;
+    case '3':
+      cantidad = base * 1.35;
+      break;
+  }
+
+  // leer el año, diferencia entre el año actual menos el ingresado
+  const diferencia = new Date().getFullYear() - this.anio;
+  // cada año de diferencia hay que reducir 3% el valor del seguro
+  cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+  /* 
+    Si el seguro es basico se multiplica por 30% más
+    Si el seguro es completo se multiplica por 50% más
+  */
+  if(this.tipo === 'basico'){
+    cantidad *= 1.30;
+  }else{
+    cantidad *= 1.50;
+  }
+
+  return cantidad;
+}
+
+
 
 // Todo lo que se muestra
 function Interfaz(){}
@@ -54,7 +96,9 @@ formulario.addEventListener('submit', function(e){
     interfaz.mostrarError('Faltan datos, revisa el formulario y prueba de nuevo.', 'error');
   }else{
     // instanciar seguro y mostrar interfaz
-    console.log('todo correcto')
+    const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo);
+    // Cotizar el seguro
+    const cantidad = seguro.cotizarSeguro(seguro);
   }
 
 });
